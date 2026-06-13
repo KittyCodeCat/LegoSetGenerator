854631fe8abbd52b4d2bfbb0ebce7541a676400f
KittyCodeCat <147434833+KittyCodeCat@users.noreply.github.com>
2026-06-13 17:24:01 +0200

    Update LegoToTuple.py so brick is a dict

--- LegoToTuple.py
+++ LegoToTuple.py
@@ -1,20 +1,28 @@
-file = open("LegoFile.txt")
-steps = [] 
+file = open("C:/Users/aberg/Documents/VS_codes/Home/10022-1_Sleeping-Ca.txt")
 
-for line in file:
-    if line.startswith("1") and line.endswith(".DAT"):
-        # Creates a tuple of (partID, position, rotation, color)
-        # in format (partID, color, (x,y,z), [[a,b,c],[d,e,f],[g,h,i]], color)
-        parts = line.split()
-        parts.pop(0) # remove 1
-        color = parts[0] # color
-        coordinates = (parts[1], parts[2], parts[3]) # x, y, z coordinates
-        rotation = [[parts[4], parts[5], parts[6]], 
-                    [parts[7], parts[8], parts[9]], 
-                    [parts[10], parts[11], parts[12]]] # rotation matrix
-        partID = parts[13] # part ID
-        brick = (partID, color, coordinates, rotation)
-        steps.append(brick)
+with open("C:/Users/aberg/Documents/VS_codes/Home/10022-1_Sleeping-Ca.txt") as file:
+    steps = [] 
+    for line in file:
+        line = line.strip()
+        if line.startswith("1") and line.endswith(".DAT"):
+            # Creates a tuple of (partID, color, position, rotation)
+            # in format (part ID, color, (x,y,z), [[a,b,c],[d,e,f],[g,h,i]])
+            parts = line.split()
+            parts.pop(0) # remove 1
+            color = parts[0] # color
+            coordinates = (parts[1], parts[2], parts[3]) # x, y, z coordinates
+            rotation = [[parts[4], parts[5], parts[6]], 
+                        [parts[7], parts[8], parts[9]], 
+                        [parts[10], parts[11], parts[12]]] # rotation matrix
+            partID = parts[13] # part ID
 
-file.close()
-print(steps)
+            brick = {"partID": partID,
+                     "color": color,
+                     "coordinates": coordinates,
+                     "rotation": rotation}
+            steps.append(brick)
+
+if steps:
+    print(steps[0])
+else:
+    print("Inga rader matchade kriterierna.")
